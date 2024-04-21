f988b571377aef432f9338ae39d297f824246721
NikhilS-IT <nikhilshaju77@gmail.com>
2024-04-21 12:43:31 +0530

    added new endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from endpoints import Endpoints
-from fetch_services import fetch_data
+from fetch_services import fetch_data, fetch_detail, fetch_video
 from filter import filter_response
 from flask_caching import Cache
 
@@ -19,6 +19,22 @@ def get_latest():
         "page" : response['page'],
         "total_pages" : response["total_pages"]
     },200)
+
+@app.route("/getmoviedetail",methods=['GET'])
+@cache.cached(timeout=300, key_prefix=lambda: request.full_path) 
+def get_movie_detail():
+    response = fetch_detail(endpoint.get_movie_detail(api_key = request.args.get('api_key'),mid = request.args.get('mid')))
+    return jsonify({
+        "result" : response
+    })
+
+@app.route("/getmoviemedia",methods=['GET'])
+@cache.cached(timeout=300, key_prefix=lambda: request.full_path) 
+def get_movie_media():
+    response = fetch_video(endpoint.get_media(api_key = request.args.get('api_key'),mid = request.args.get('mid'), media_type = 'video'))
+    return jsonify({
+        "result" : response
+    })
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
