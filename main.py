34412b4e1cd436dad432f768facb37aad0e3aa21
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2024-04-29 22:32:53 +0530

    added new access points

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from endpoints import Endpoints
-from fetch_services import fetch_data, fetch_detail, fetch_video
-from filter import filter_response, filter_tv
+from fetch_services import fetch_cast_and_crew, fetch_data, fetch_detail, fetch_images, fetch_video
+from filter import filter_response, filter_tv, filter_tv_detail
 from flask_caching import Cache
 
 endpoint = Endpoints()
@@ -27,20 +27,56 @@ def get_latest():
 @app.route("/getmoviedetail", methods=['GET'])
 @cache.cached(timeout=300, key_prefix=lambda: request.full_path)
 def get_movie_detail():
-    response = fetch_detail(endpoint.get_movie_detail(
+    details = fetch_detail(endpoint.get_movie_detail(
         api_key=request.args.get('api_key'), mid=request.args.get('mid')))
+    video = fetch_video(endpoint.get_media(api_key=request.args.get(
+        'api_key'), mid=request.args.get('mid'), media_type='video'))
+    image = fetch_images(endpoint.get_media(api_key=request.args.get(
+        'api_key'), mid=request.args.get('mid'), media_type='image'))
+    cast_and_crew = fetch_cast_and_crew(endpoint.get_cast_and_crew(api_key=request.args.get(
+        'api_key'), mid=request.args.get('mid')))
     return jsonify({
-        "result": response
+        'title': details['title'],
+        'overview': details['overview'],
+        'duration': details['duration'],
+        'genres': details['genres'],
+        'release_date': details['release_date'],
+        'rating': details['rating'],
+        'lang': details['lang'],
+        'poster_path': image['poster'],
+        'backdrop_path': image['backdrop'],
+        'video': video['key'],
+        'site': video['site'],
+        'type': 'movie',
+        'cast': cast_and_crew['cast'],
+        'crew': cast_and_crew['crew']
     })
 
 
-@app.route("/getmoviemedia", methods=['GET'])
+@app.route("/gettvdetail", methods=['GET'])
 @cache.cached(timeout=300, key_prefix=lambda: request.full_path)
-def get_movie_media():
-    response = fetch_video(endpoint.get_media(api_key=request.args.get(
-        'api_key'), mid=request.args.get('mid'), media_type='video'))
+def get_tv_detail():
+    response = fetch_data(endpoint.get_tv_detail(
+        api_key=request.args.get('api_key'), tid=request.args.get('tid')))
+    filter_response = filter_tv_detail(
+        api_key=request.args.get('api_key'), response=response, tid=request.args.get('tid'))
+    cast_and_crew = fetch_cast_and_crew(url=endpoint.get_tv_credits(
+        api_key=request.args.get('api_key'), tid=request.args.get('tid')))
     return jsonify({
-        "result": response
+        "video": filter_response['video'],
+        "site": filter_response['site'],
+        "type": 'tv',
+        'poster_path': filter_response['poster_path'],
+        'backdrop_path': filter_response['backdrop_path'],
+        "id": filter_response["id"],
+        "title": filter_response["title"],
+        "overview": filter_response["overview"],
+        "genres": filter_response["genres"],
+        "lang":  filter_response["lang"],
+        "release_date": filter_response["release_date"],
+        "rating": filter_response["rating"],
+        'cast': cast_and_crew["cast"],
+        'crew': cast_and_crew['crew'],
     })
 
 
@@ -49,15 +85,17 @@ def get_movie_media():
 def get_latest_tv():
     response = fetch_data(endpoint.trendingTV,
                           params={
-                              "api_key": request.args.get('api_key')
-                          },
-                          )
+                              "api_key": request.args.get('api_key'),
+                              "page": request.args.get('page', default=1, type=int)
+                          })
 
-    filter_response = filter_tv(api_key= request.args.get('api_key'),response=response)
+    filter_response = filter_tv(
+        api_key=request.args.get('api_key'), response=response)
 
     return jsonify({
         "result": filter_response
     })
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
